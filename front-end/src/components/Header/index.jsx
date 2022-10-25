f22e6ea3783a95a9f17cfaad9b3ce4ca29b23b25
tunim73 <antonio.netto973@gmail.com>
2022-10-25 17:10:53 -0300

    separando componentes do header 2 - menu Hamburguer

--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import style from './Header.module.scss'
 import MenuTexto from './header components/MenuTexto';
+import MenuHamburguer from './header components/MenuHamburguer';
 
 
 
@@ -18,52 +19,7 @@ const Header = () => {
         <>  
             <header className = {style.cabeçalho}>
                 
-                <div className= {style.container}>
-        
-                    <input type="checkbox" id="menu" className={style.container__botao}/>
-
-                        <label htmlFor='menu' className={style.container__rotulo}>
-                            <span className={style.cabeçalho__menu_hamburguer} ></span>
-                        </label>
-                        
-                        <ul className={style.lista_menu}>
-                            <li className={style.lista_menu__titulo}>
-                                Categorias
-                            </li>
-                            <li className={style.lista_menu__item}>
-                                <a href="#" className={style.lista_menu__link}>
-                                    Entrar/Cadastra-se
-                                </a>
-                            </li>
-                            <li className={style.lista_menu__item}>
-                                <a href="#" className={style.lista_menu__link}>
-                                    Home
-                                </a>
-                            </li>
-                            <li className={style.lista_menu__item}>
-                                <a href="#" className={style.lista_menu__link}>
-                                    Categorias
-                                </a>
-                            </li>
-                            <li className={style.lista_menu__item}>
-                                <a href="#" className={style.lista_menu__link}>
-                                    Autores de Destaque
-                                </a>
-                            </li>
-                            <li className={style.lista_menu__item}>
-                                <a href="#" className={style.lista_menu__link}>
-                                    Carrinho de compras
-                                </a>
-                            </li>
-                        </ul>
-
-                        <img src="/public/assets/imagens/logo.png" alt="Logo da Crônicas e Papel" className={style.container__imagem}/>
-                        <h1 className={style.container__titulo}>
-                            <b className={style.container__titulo_negrito }>Crônicas&</b>Papel
-                        </h1>
-                        
-                </div>
-
+                <MenuHamburguer/>
                 <MenuTexto/>
                 
                 <div className={style.container}>           

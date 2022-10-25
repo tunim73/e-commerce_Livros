43d794e2af9c64ad3c4db85102e3b0843d629b32
tunim73 <antonio.netto973@gmail.com>
2022-10-25 17:19:38 -0300

    separando componentes do header 2 - icons

--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import style from './Header.module.scss'
 import MenuTexto from './header components/MenuTexto';
 import MenuHamburguer from './header components/MenuHamburguer';
+import Icons from './header components/Icons';
 
 
 
@@ -21,24 +22,8 @@ const Header = () => {
                 
                 <MenuHamburguer/>
                 <MenuTexto/>
+                <Icons/>
                 
-                <div className={style.container}>           
-                    <a href="#">
-                        <img src="/public/assets/imagens/favorito.png" alt="Meus favoritos" className={style.container__imagem_transparente}/>
-                    </a>
-                    <a href="#" className={style.container__link}>
-                        <img src="/public/assets/imagens/carrinho.png" alt="Carrinhos de compras" className={style.container__imagem} />
-                        <p className={style.container__texto}>
-                            Minha sacola
-                        </p>
-                    </a>
-                    <a href="#" className={style.container__link}>
-                        <img src="/public/assets/imagens/user.png" alt="Meu Perfil" className={style.container__imagem}/>
-                        <p className={style.container__texto}>
-                            Meu Perfil
-                        </p>
-                    </a>
-                </div>
 
             </header>
         </>

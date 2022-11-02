ddc81135c9433ceddf2aabbeaae1508948985e76
Guaricaya <rafaelbarros240@gmail.com>
2022-11-02 15:13:23 -0300

    Pagina de Detalhes de Livro

---
+++ front-end/src/components/Titulo/index.jsx
@@ -0,0 +1,16 @@
+import style from './Titulo.module.scss'
+
+
+const Titulo = (props) => {
+
+
+    return ( 
+        <div className = { style.container }>
+                <div >
+                        <h1>{props.forNome}</h1>
+                </div>
+                
+        </div> 
+        )
+}
+export default Titulo

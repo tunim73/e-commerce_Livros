58bfdc1b610c70f01cc17544f6d7acd920e5de1f
Chahoud1 <roger_chahoud@outlook.com>
2022-11-06 19:52:50 -0300

    modificacao no detalhes do livro

--- front-end/src/pages/DetalhesAutor/index.jsx
+++ front-end/src/pages/DetalhesAutor/index.jsx
@@ -9,9 +9,12 @@ const DetalhesLivro = () => {
     return ( 
     <>
         <Header/>
-        <Titulo forNome = "Livro"/>
-        <Sinopse forNome = "Sinopse" forResumo = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."/>
-        
+
+        <div className={style.container}>
+          <Titulo forNome = "Autor"/>
+          <img className={style.img} src="https://t.ctcdn.com.br/essK16aBUDd_65hp5umT3aMn_i8=/400x400/smart/filters:format(webp)/i606944.png" />
+          <Sinopse forNome = "Biografia" forResumo = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."/>
+        </div>
         <Footer/>
     </>
     )
--- front-end/src/pages/DetalhesLivro/index.jsx
+++ front-end/src/pages/DetalhesLivro/index.jsx
@@ -5,18 +5,22 @@ import Footer from '../../components/Footer'
 import Titulo from '../../components/Titulo'
 import Sinopse from '../../components/Sinopse'
 import {Botao} from '../../components/Botao'
+
 const DetalhesLivro = () => {
     return ( 
     <>
         <Header/>
-        <Titulo forNome = "Livro"/>
         <Banner/>
+        <Titulo forNome = "Livro"/>
+        
         <div className={style.wrap}>
             <h1 className={style.preco}>Preço : R$20,00</h1>
+            <img className={style.img} src='https://www.carochinhaeditora.com.br/wp-content/uploads/2021/01/Ah-nao_E01_CAPA-V1.png' />
             <Botao className ={style.botao} botao="Comprar"/>
+            <Sinopse forNome = "Sinopse" forResumo = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."/>
         </div>
-        <Sinopse forNome = "Sinopse" forResumo = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."/>
         
+         
         <Footer/>
     </>
     )
